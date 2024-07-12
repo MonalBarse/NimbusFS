281a9e74195ebae674692707af071da8f2ace5da
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-07-13 04:14:24 +0530

    build: to run and check the flow of NimbusFS

--- main.go
+++ main.go
@@ -1,37 +1,72 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/MonalBarse/NimbusFS/p2p"
 )
 
-func OnPeer(p p2p.Peer) error {
-	p.Close()
-	fmt.Println("doing somting with peer outside of TCPTransport")
-	return nil
-}
-
-func main() {
-	tcpOptions := p2p.TCPTransportOptions{
-		ListenAddress: ":8080",
+func makeServer(listenAddr string, nodes ...string) *FileServer {
+	tcptransportOpts := p2p.TCPTransportOptions{
+		ListenAddress: listenAddr,
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
 	}
-	fmt.Println("Hello, Nimbus")
-	tr := p2p.NewTCPTransport(tcpOptions)
 
-	go func() {
-		for {
-			rpc := <-tr.Consume()
-			fmt.Println("Received message: ", string(rpc.Payload))
-		}
-	}()
+	tcpTransport := p2p.NewTCPTransport(tcptransportOpts)
 
-	if err := tr.ListenAndAccept(); err != nil {
-		log.Fatalf("failed to listen and accept: %+v", err)
-
+	fileServerOpts := FileServerOpts{
+		EncKey:            newEncryptionKey(),
+		StorageRoot:       listenAddr + "_network",
+		PathTransformFunc: CASPathTransformFunc,
+		Transport:         tcpTransport,
+		BootstrapNodes:    nodes,
 	}
-	select {}
+
+	s := NewFileServer(fileServerOpts)
+
+	tcpTransport.OnPeer = s.OnPeer
+
+	return s
+}
+
+func main() {
+	s1 := makeServer(":3000", "")
+	s2 := makeServer(":7000", "")
+	s3 := makeServer(":5000", ":3000", ":7000")
+
+	go func() { log.Fatal(s1.Start()) }()
+	time.Sleep(500 * time.Millisecond)
+	go func() { log.Fatal(s2.Start()) }()
+
+	time.Sleep(2 * time.Second)
+
+	go s3.Start()
+	time.Sleep(2 * time.Second)
+
+	for i := 0; i < 20; i++ {
+		key := fmt.Sprintf("picture_%d.png", i)
+		data := bytes.NewReader([]byte("my big data file here!"))
+		s3.Store(key, data)
+
+		if err := s3.store.Delete(s3.ID, key); err != nil {
+			log.Fatal(err)
+		}
+
+		r, err := s3.Get(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, err := io.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(b))
+	}
 }
